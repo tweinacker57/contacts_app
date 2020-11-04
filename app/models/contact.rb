f69ddf527907fc35a5f89840e3b9bd908eebc2a6
Tyler Weinacker <tweinacker57@gmail.com>
2020-11-03 20:04:06 -0500

    added 3 contacts to the database with first name, last name, email, and phone number

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,2 @@
-class Contact
-  attr_accessor :first_name, :last_name, :email, :phone_number
-
-  def initialize(input_options)
-    @first_name = input_options[:first_name]
-    @last_name = input_options[:last_name]
-    @email = input_options[:email]
-    @phone_number = input_options[:phone_number]
-  end
+class Contact < ApplicationRecord
 end
